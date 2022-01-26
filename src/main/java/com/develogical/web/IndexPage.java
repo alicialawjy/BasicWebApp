16087e19318f0334ac9b1dda65cbe3e372e9f1b8
alicialawjy <alicialawjy@gmail.com>
2022-01-26 23:33:27 +0000

    modified index page

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome to Alicia's Webpage!!</h1>" +
+                "<h1>Welcome to Alicia's Webpage!!!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
